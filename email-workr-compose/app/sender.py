d548a42a229881cd2faa1f5b0d251d6f78b1caf3
unimedalbatista <97983109+unimedalbatista@users.noreply.github.com>
2023-02-04 08:49:42 -0300

    passo 4 Criação do app

---
+++ email-workr-compose/app/sender.py
@@ -0,0 +1,10 @@
+from bottle import route, run, request
+
+@route('/', method='POST')
+def send():
+    assunto = request.forms.get('assunto')
+    messagem = request.forms.get('mensagem')
+    return 'mensagem enfileirada! Assunto {} Mensagem: {}'.format(assunto,messagem)
+
+if __name__=='__main__':
+    run(host='0.0.0.0', port=8080, debug=True)
